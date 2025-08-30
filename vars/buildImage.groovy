ecca324711513a22a37d11775cf11fd0d8c72623
Admin <admin@gmail.com>
2025-08-30 12:48:19 +0000

    Update vars/buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-def call {
+def call() {
     echo "building the docker image..."
     sh 'docker build -t waqassaleem/java-maven-app:4.0 .'
     sh 'docker tag waqassaleem/java-maven-app:4.0 localhost:90/java-maven-app:4.0'
