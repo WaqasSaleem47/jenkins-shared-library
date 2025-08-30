c2235ee69a2f83952ccb9f95df534e9de103359a
Waqas Saleem <waqassaleem47@protonmail.com>
2025-08-31 01:31:41 +0500

    multiple functions

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -9,7 +9,16 @@ class Docker implements Serializable {
     }
     def buildDockerImage(String imageName) {
         script.echo "building the docker image..."
-        script.sh "docker build -t $imageName ."
-        script.sh "docker tag $imageName localhost:90/java-maven-app:4.0"
+        script.sh "docker build -t waqassaleem/$imageName ."
+        script.sh "docker tag waqassaleem/$imageName localhost:90/$imageName"
+    }
+    def dockerLogin(){
+        script.withCredentials([script.usernamePassword(credentialsId: 'dockerHub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+        }
+    }
+    def dockerPush(String imageName){
+        script.sh "docker push waqassaleem/$imageName"
+        script.sh "docker push localhost:90/$imageName"
     }
 }
