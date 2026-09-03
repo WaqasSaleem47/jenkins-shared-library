6ebc2b74be3d4ada2ceb66f0383743b7211138cd
Waqas Saleem <202511761+WaqasSaleem47@users.noreply.github.com>
2026-09-04 03:26:51 +0500

    Remove localhost tagging and pushing for Docker images
    Removed tagging and pushing to localhost for Docker images.

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -10,7 +10,6 @@ class Docker implements Serializable {
     def buildDockerImage(String imageName) {
         script.echo "building the docker image..."
         script.sh "docker build -t waqassaleem/$imageName ."
-        script.sh "docker tag waqassaleem/$imageName localhost:90/$imageName"
     }
     def dockerLogin(){
         script.withCredentials([script.usernamePassword(credentialsId: 'dockerHub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
@@ -19,6 +18,5 @@ class Docker implements Serializable {
     }
     def dockerPush(String imageName){
         script.sh "docker push waqassaleem/$imageName"
-        script.sh "docker push localhost:90/$imageName"
     }
 }
